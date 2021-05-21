d0ff74a5e71b54d403c07c365fc68e7ebf6d6228
Noda-0110 <hj-g20024@sist.ac.jp>
2021-05-21 11:42:14 +0900

    game.hを作った ヘッダ読み込みを変更

--- main.cpp
+++ main.cpp
@@ -1,28 +1,8 @@
-//ヘッダーファイルの読み込み
-#include "DxLib.h"		//DxLibを使う時に必要
+
+//ヘッダファイル読み込み
+#include "game.h"		//ゲームを管理する為のヘッダファイル　あくまでもゲームの管理
 #include "keyboard.h"	//キーボードの処理
 #include "FPS.h"		//FPSの処理
-
-
-//マクロ定義
-#define GAME_TITLE "ゲームタイトル"	//ゲームタイトル
-#define GAME_WIDTH  1280		//ゲーム画面の幅
-#define GAME_HEIGHT 720			//ゲーム画面の高さ
-#define GAME_COLOR  32			//ゲームの色域
-
-#define GAME_ICON_ID 333			//ゲームのICONのID
-
-
-#define GAME_WINDOW_BAR 0	//ウインドウバーの種類
-
-
-//列挙型
-enum GAME_SCENE {
-	GAME_SCENE_TITLE,
-	GAME_SCENE_PLAY,
-	GAME_SCENE_END,
-	GAME_SCENE_CHANGE //エフェクトを出すシーン
-};//ゲームのシーン
 
 //グローバル変数
 //シーンを管理する変数
